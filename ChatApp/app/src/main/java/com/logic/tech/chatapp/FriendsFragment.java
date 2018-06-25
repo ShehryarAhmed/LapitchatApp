6588a137ac593f70d1efca1405a03321574af86c
shehryarahmed <shehreyarahmed5@gmail.com>
2018-06-26 00:34:22 +0500

    friends fragment

--- ChatApp/app/src/main/java/com/logic/tech/chatapp/FriendsFragment.java
+++ ChatApp/app/src/main/java/com/logic/tech/chatapp/FriendsFragment.java
@@ -3,9 +3,14 @@ package com.logic.tech.chatapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -13,6 +18,11 @@ import android.view.ViewGroup;
  */
 public class FriendsFragment extends Fragment {
 
+    private String mCurrent_user_id;
+    private View mMainView;
+    private RecyclerView mFriendList;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mFriendsDatabase;
 
     public FriendsFragment() {
         // Required empty public constructor
@@ -22,6 +32,15 @@ public class FriendsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        mMainView = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        mFriendList = (RecyclerView) mMainView.findViewById(R.id.friends_list);
+        mAuth = FirebaseAuth.getInstance();
+
+        mCurrent_user_id = mAuth.getCurrentUser().getUid();
+
+        mFriendsDatabase = FirebaseDatabase.getInstance().getReference().child("Friends").child(mCurrent_user_id);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_friends, container, false);
     }
